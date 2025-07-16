35ae9cd74f1253633a9aa1a404b0afb9b8d07afd
DanieleAmodio <amodio_daniele8990@live.it>
2025-07-16 22:16:24 +0200

    ok2

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MyNav from './components/MyNav.jsx'
+import Welcome from './components/Welcome.jsx'
+import Footer from './components/Footer.jsx'
+import AllTheBooks from './components/AllTheBooks.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <>
+  <MyNav />
+  <Welcome />
+  <AllTheBooks />
+   <Footer />
+ 
+  </>
+   )
